a655cdeb1eb2b802c7efcad7417b3233f1dc5b9c
alonso-estevam <alonso_estevam@outlook.com>
2022-07-13 17:26:13 -0300

    feat: import prompt-sync module

--- script.js
+++ script.js
@@ -11,3 +11,9 @@ O código deve rodar em um laço que vai receber propriedades até a pessoa digi
 // Primeiro, inicializei o pacote do projeto com o comando "npm init", para criar o arquivo package.json.
 // Depois, no arquivo package.json inseri o script "type":"module", para permitir a forma mais atualizada de importação
 // Em seguida, executei o comando "npm install prompt-sync" no terminal, para instalar o pacote que possibilita ao terminal do node receber inputs da pessoa usuária. Essa ação criou a pasta node_modules, que não deve ser subida para o github
+
+// Para importar o pacote prompt-sync, adicionei o seguinte comando:
+import promptSync from 'prompt-sync';
+
+// O módulo prompt-sync é uma função que cria funções de prompt, portanto, é preciso "chamá-lo", adicionando os parênteses após o nome da função: promptSync(). Fiz isso e criei uma constante para armazená-lo:
+const prompt = promptSync();
